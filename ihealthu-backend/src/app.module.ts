64d06218986c20796e2e80e95b9222631fe439cb
rafaabennour14@gmail.com <rafaabennour14@gmail.com>
2026-05-04 16:50:13 +0100

    Add Activities module, controller, and service with tests

--- ihealthu-backend/src/app.module.ts
+++ ihealthu-backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ActivitiesModule } from './activities/activities.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ActivitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
